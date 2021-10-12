ca2e6727b1400e9734818c68a5a16c43b5b580ad
Koala <karkalvatas@yandex.ua>
2021-10-12 15:10:10 +0300

    Portfolio
    portfolio company

---
+++ script/loadPortfolio.js
@@ -0,0 +1,335 @@
+let mesPortfolio = [
+  {
+    img: `images/diary.png`,
+    title: `Зашифрованный электронный дневник`,
+    data: 0,
+    github: `https://anteyku.github.io/game/`,
+    link: `https://vgtimes.ru/`,
+    description: `Электронный дневник с авторизацией и регистрацией, шифрует все ваши записи в нем. Парсит самые актуальные новости с сайта https://www.ferra.ru/ и выводит на главную страничку, а также показывает прогноз погоды в Украине, городе Киев. Помимо этого дневник умеет распознавать количество дней в текущем месяце, а также дописывать оставшиеся количество. При создании дня вы можете выбрать одну из трех стилизаций отображаемых в дневника. Также можно удалить, редактировать, добавлять записи. Адаптивен под все телефонные устройства и планшеты.`,
+    slider: [
+      `images/diary.png`,
+      `images/dineryAuthh.png`,
+      `images/diaryReg.png`
+    ],
+    skills: [
+      {
+        image: `images/html.png`,
+        title: `HTML5`
+      },
+      {
+        image: `images/css3.png`,
+        title: `CCS3`        
+      },
+      {
+        image: `images/javascript.png`,
+        title: `JavaScript`        
+      },
+      {
+        image: `images/node.png`,
+        title: `Node.js`        
+      },
+      {
+        image: `images/mongo.png`,
+        title: `MongoDB`        
+      },
+      {
+        image: `images/less.png`,
+        title: `Less`        
+      }      
+    ]
+  },
+  {
+    img: `images/anime.png`,
+    title: `Видео хостинг - Аниме`,
+    data: 1,
+    github: `https://anteyku.github.io/game/`,
+    link: `https://vgtimes.ru/`,
+    description: `Сверстанный аниме форум, предназначенный для просмотра аниме. Также реализованный мелкие детали типа слайдера и воспроизведения видео клипов на главной странице на клиентском javascript. Дизайн придуман и создан нами, на момент написания данного проекта у нас стояла цель - реализовать дизайн сложного и масштабного форума. Адаптивен под все телефонные устройства и планшеты.`,
+    slider: [
+      `images/anime.png`,
+      `images/animeTWO.png`,
+      `images/animeTHRRE.png`,
+      `images/animeFOR.png`
+    ],
+    skills: [
+      {
+        image: `images/html.png`,
+        title: `HTML5`
+      },
+      {
+        image: `images/css3.png`,
+        title: `CCS3`        
+      },
+      {
+        image: `images/javascript.png`,
+        title: `JavaScript`        
+      },
+      {
+        image: `images/less.png`,
+        title: `Less`        
+      }      
+    ]        
+  },
+  {
+    img: `images/shopkey.png`,
+    title: `Магазин игровых ключей`,
+    data: 2,
+    github: `https://anteyku.github.io/game/`,
+    link: `https://vgtimes.ru/`,
+    description: `Сверстанный макет - Магазина лицензионных ключей. Дизайн создан нами. Реализован скрипт слайдера, а также рандом рулетки игор. Создан с целю практики в версте масштабных проектов и создания дизайна. Имеет 4 уникальных страницы. Адаптивен под все телефонные устройства и планшеты.`,
+    slider: [
+      `images/game.png`,
+      `images/gameTwo.png`,
+      `images/gameThree.png`,
+      `images/gameFor.png`
+    ],
+    skills: [
+      {
+        image: `images/html.png`,
+        title: `HTML5`
+      },
+      {
+        image: `images/css3.png`,
+        title: `CCS3`        
+      },
+      {
+        image: `images/javascript.png`,
+        title: `JavaScript`        
+      },
+      {
+        image: `images/less.png`,
+        title: `Less`        
+      }      
+    ]        
+  },
+  {
+    img: `images/christmasFOR.png`,
+    title: `Новогодний сайт подарков`,
+    data: 3,
+    github: `https://anteyku.github.io/game/`,
+    link: `https://vgtimes.ru/`,
+    description: `Новогодний сайт подарков - идея в том что близкие покупают неизвестный для себя подарок родному человеку. Дизайн создан нами. Макет только сверстан. Адаптивен под все телефонные устройства и планшеты.`,
+    slider: [
+      `images/christmas.png`,
+      `images/christmasTwo.png`,
+      `images/christmasThree.png`,
+      `images/christmasFOR.png`
+    ],
+    skills: [
+      {
+        image: `images/html.png`,
+        title: `HTML5`
+      },
+      {
+        image: `images/css3.png`,
+        title: `CCS3`        
+      },
+      {
+        image: `images/less.png`,
+        title: `Less`        
+      }      
+    ]        
+  },
+  {
+    img: `images/csgoFive.png`,
+    title: `Магазин CS-GO кейсов `,
+    data: 4,
+    github: `https://anteyku.github.io/game/`,
+    link: `https://vgtimes.ru/`,
+    description: `Чистая верстка магазина cs-go кейсов. Дизайн сделан нами. Макет сверстан за неделю. При его создании все картинки предметов в кейсах были взяты из открытого доступа в интернете. Сайт предназначался для открытия кейсов cs-go. Адаптивен под все телефонные устройства и планшеты.`,
+    slider: [
+      `images/csgoTwo.png`,
+      `images/csgoThree.png`,
+      `images/csgoFor.png`,
+      `images/csgoFive.png`
+    ],
+    skills: [
+      {
+        image: `images/html.png`,
+        title: `HTML5`
+      },
+      {
+        image: `images/css3.png`,
+        title: `CCS3`        
+      },
+      {
+        image: `images/less.png`,
+        title: `Less`        
+      }      
+    ]        
+  },
+  {
+    img: `images/youtubeTwo.png`,
+    title: `Сайт визитка для ютубера`,
+    data: 5,
+    github: `https://anteyku.github.io/game/`,
+    link: `https://vgtimes.ru/`,
+    description: `Сайт сверстан для рассказчика страшных историй Дмитрий Ray. Дизайн создан нами. Сам канал https://www.youtube.com/channel/UCYcygnLe4cCuo2Zx0jtkcyg. Адаптивен под все телефонные устройства и планшеты.`,
+    slider: [
+      `images/youtubeTwo.png`,
+      `images/youtubeThree.png`,
+      `images/youtubeFor.png`,
+      `images/youtubeFive.png`
+    ],
+    skills: [
+      {
+        image: `images/html.png`,
+        title: `HTML5`
+      },
+      {
+        image: `images/css3.png`,
+        title: `CCS3`        
+      },
+      {
+        image: `images/less.png`,
+        title: `Less`        
+      }      
+    ]        
+  }           
+]
+
+
+
+for(let i = 0; i < mesPortfolio.length; i++){
+  createElem(mesPortfolio[i].img, mesPortfolio[i].title, mesPortfolio[i].data, i, true);
+}
+
+
+function createElem(img, name, data, num, first){
+  
+  let portfolio = document.querySelector(`.portfolio__project`);
+
+  let block = document.createElement(`div`);
+  block.classList.add(`portfolio__project__block`);
+  block.dataset.num = data;
+  
+  let blockImage = document.createElement(`div`);
+  blockImage.classList.add(`portfolio__project__block__image`);
+
+  let blockImageIMG = document.createElement(`img`);
+  blockImageIMG.dataset.src = img;
+  blockImageIMG.classList.add(`lazyload`);
+
+ let blockTitle = document.createElement(`div`);
+ blockTitle.classList.add(`portfolio__project__block__title`);
+ blockTitle.innerHTML = name;
+
+ blockImage.appendChild(blockImageIMG);
+ block.appendChild(blockImage);
+ block.appendChild(blockTitle);
+
+ if(num > 2 && first == true){
+  block.style.display = `none`;
+ }
+
+ portfolio.appendChild(block);
+}
+
+
+
+// Закрытия портфолио
+for(let elem of document.querySelectorAll(`.modulePortfolio__close`)){
+  elem.addEventListener(`click`, (event)=>{
+    event.path[2].style.display = `none`;
+    
+    // Удаления всех картинок в поле "СКРИНШОТЫ" в модульном оккне
+    for(let elem of document.querySelectorAll(`.modulePortfolio__screen__slider__img`)){
+      document.querySelector(`.modulePortfolio__screen__slider`).removeChild(elem);
+    }
+
+    // Удаления всех скилов в поле "ПРИМЕНЯЕМЫЕ НАВЫКИ" в модульном окне
+    for(let elem of document.querySelectorAll(`.skil`)){
+      document.querySelector(`.skill`).removeChild(elem);
+    }    
+
+    document.querySelector(`html`).style.overflowY = `scroll`;
+    
+  })
+}
+
+
+// Открытия модульного окна при нажатии на проект
+let modalPort = document.querySelector(`.modulePortfolio`);
+let modalTitle = document.querySelector(`.modulePortfolio__top__title`);
+let gitHub = document.querySelector(`.modulePortfolio__top__github`);
+let linkSite = document.querySelector(`.modulePortfolio__top__button`);
+let description = document.querySelector(`.modulePortfolio__description__about`);
+let sliders = document.querySelector(`.modulePortfolio__screen__slider`);
+let blockSkill = document.querySelector(`.skill`);
+
+for(let elem of document.querySelectorAll(`.portfolio__project__block`)){
+  elem.addEventListener(`click`, (event)=>{
+    document.querySelector(`html`).style.overflow = `hidden`;
+    // Делаим видимым модульное окно
+    modalPort.style.display = `block`;
+    // Ставим всю инфу из нашего массива mesPortfolio
+    modalTitle.innerHTML = mesPortfolio[elem.dataset.num].title;
+    gitHub.href = mesPortfolio[elem.dataset.num].github;
+    linkSite.href = mesPortfolio[elem.dataset.num].link;
+    description.innerHTML = mesPortfolio[elem.dataset.num].description;
+
+    
+    // Добавляем скриншоты сайта в модульное окно
+    for(let i = 0; i <  mesPortfolio[elem.dataset.num].slider.length; i++){
+      let layzeLoad = document.createElement(`div`);
+      layzeLoad.classList.add(`modulePortfolio__screen__slider__img`);
+      let layzeLoadIMG = document.createElement(`img`);
+      layzeLoadIMG.classList.add(`lazyload`);
+      layzeLoadIMG.dataset.src = mesPortfolio[elem.dataset.num].slider[i];
+      layzeLoad.appendChild(layzeLoadIMG);
+      sliders.appendChild(layzeLoad);
+    }
+
+    // Добавления всех скилов
+    for(let i = 0; i <  mesPortfolio[elem.dataset.num].skills.length; i++){
+      let Kenta = document.createElement(`div`);
+      Kenta.classList.add(`moduleAbout__skills__langues__image`);
+      let KentaIMG = document.createElement(`img`);
+      KentaIMG.classList.add(`lazyload`);
+      KentaIMG.dataset.src = mesPortfolio[elem.dataset.num].skills[i].image;  
+      let skillText = document.createElement(`div`);
+      skillText.classList.add(`moduleAbout__skills__langues__text`);
+      skillText.innerHTML = mesPortfolio[elem.dataset.num].skills[i].title;
+      let skillsL = document.createElement(`div`);
+      skillsL.classList.add(`moduleAbout__skills__langues`);
+      skillsL.classList.add(`skil`);
+      Kenta.appendChild(KentaIMG);
+      skillsL.appendChild(Kenta);
+      skillsL.appendChild(skillText);
+      blockSkill.appendChild(skillsL);
+      
+    }  
+
+
+  })
+}
+
+
+
+
+// Нажатия на кнопку "Загрузить еще"
+let loadY = document.querySelector(`.loadYet`);
+let SldLoad = document.querySelector(`.portfolio`);
+loadY.addEventListener(`click`, ()=>{
+  for(let elem of document.querySelectorAll(`.portfolio__project__block`)){
+    console.log(elem.dataset);
+    if(elem.dataset.num > 2){
+      elem.style.display = `block`;
+    }
+  }
+
+  SldLoad.removeChild(loadY);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
