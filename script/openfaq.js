966eb60bfedc38ce189768ac7e318ed4ce6c7b47
anteyku <karkalvatas@yandex.ru>
2022-10-31 20:29:07 +0200

    update design
    update design

--- script/openfaq.js
+++
@@ -1,8 +0,0 @@
-let mesFaq = document.querySelectorAll(`.faq__block`);
-
-// При нажатии вопрос из блока FAQ скрипт развертывает ответ
-for(let i = 0; i < mesFaq.length; i++){
-  mesFaq[i].addEventListener(`click`, (event)=>{
-    mesFaq[i].children[2].style.display = `block`;
-  })
-}
